bdb498c2987615fbb18c56a90c80a1dfb8930ba6
Linhnguyen02 <ngglinh0914@gmail.com>
2022-06-17 21:17:33 +0700

    update tine

---
+++ index.js
@@ -0,0 +1,30 @@
+const day = document.querySelector("#days")
+const hours = document.querySelector("#hours")
+const minutes = document.querySelector("#minutes")
+const second = document.querySelector("#second")
+console.log(day,hours,minutes,second)
+const newDay = 'July 20, 2022'
+function timeCountDown() {
+    const nowDate = new Date().getTime();
+    const newYearDate = new Date(newDay).getTime();
+    
+    const gap = newYearDate - nowDate
+    
+    const sc= 1000;
+    const mn = sc * 60
+    const hour = mn * 60
+    const days = hour * 24
+    const textDay = Math.floor(gap/days)
+    const textHour = Math.floor((gap % days) / hour)
+    const textMinute = Math.floor((gap % hour) / mn)
+    const textSecond = Math.floor((gap % mn) /sc)
+    
+    day.innerHTML = textDay
+    hours.innerHTML = textHour
+    minutes.innerHTML = textMinute
+    second.innerHTML = textSecond
+  }
+  
+  
+  timeCountDown()
+  
